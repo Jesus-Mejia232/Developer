b25dafb60c3b60fe5252e29d16dc13507718459c
jesus-mejia232 <jesusmmarco00@gmail.com>
2023-08-14 22:02:29 -0600

    Actuali

---
+++ POO/Decoradores/Concepto.t
@@ -0,0 +1,5 @@
+Los decoradores son funciones que decoran a otra
+Ejemplo;
+
+Si yo tengo una función que dice "Hablar" y tengo a otra funcion que "Decora" a esa funcion es porque es una funcion
+
---
+++ POO/Decoradores/decora.py
@@ -0,0 +1,19 @@
+def holamundo(saludando):
+    def presentarme():
+        print("Antes de llamar la función")
+        saludando()
+        print("Después de llamaar a la función")
+    return presentarme
+
+#
+
+
+
+
+
+
+
+
+
+
+
